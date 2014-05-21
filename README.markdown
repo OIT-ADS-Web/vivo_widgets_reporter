657d2e2f1ad6fed52ad8d995d8b1578abb8d3618
Patrick McElwee <pmm21@duke.edu>
2014-05-21 16:57:50 -0400

    Separate dev and prod environments

--- README.markdown
+++ README.markdown
@@ -1,3 +1,11 @@
+## Setup
+
 Install [Leiningen](http://leiningen.org). If you are using Homebrew on a Mac, you can run:
 
     brew install leiningen
+
+Compile the files:
+
+    git clone https://github.com/OIT-ADS-Web/vivo_widgets_reporter.git
+    cd vivo_widgets_reporter
+    lein cljsbuild auto development
--- project.clj
+++ project.clj
@@ -1,21 +1,31 @@
 (defproject vivo_widgets_reporter "0.1.0-SNAPSHOT"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
+  :description "Reports for individual scholars, pulling data from Vivo Widgets."
+  :url "https://github.com/OIT-ADS-Web/vivo_widgets_reporter"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.5.0"]]
+                 [om "0.6.2"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
-    :builds [{:id "vivo_widgets_reporter"
+  :cljsbuild {
+    :builds [{:id "development"
               :source-paths ["src"]
               :compiler {
-                :output-to "vivo_widgets_reporter.js"
-                :output-dir "out"
+                :output-to "vivo_widgets_reporter.development.js"
+                :output-dir "out/development"
                 :optimizations :none
-                :source-map true}}]})
+                :source-map true}}
+             {:id "production"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "vivo_widgets_reporter.production.js"
+                :output-dir "out/production"
+                :optimizations :advanced
+                :pretty-print false
+                :preamble ["react/react.min.js"]
+                :externs ["react/externs/react.js"]}}
+             ]})
