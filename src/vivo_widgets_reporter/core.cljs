0d8fcd372d309c0668abe3b29abb71fd567a1150
Patrick McElwee <pmm21@duke.edu>
2014-05-22 15:09:53 -0400

    First updates to the DOM
    Can specify whether 'overview' should appear or not

--- src/vivo_widgets_reporter/core.cljs
+++ src/vivo_widgets_reporter/core.cljs
@@ -1,13 +1,53 @@
 (ns vivo_widgets_reporter.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]
+            ))
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello world!"}))
+(def app-state (atom {
+  :faculty-uri "https://scholars.duke.edu/individual/per2051062"
+  :include-overview true
+  }))
+
+(defn generate-report [{:keys [include-overview]}]
+  (if include-overview
+    "overview here"
+    "overview not here"
+    )
+  )
+
+(defn update-preference [e owner preference]
+  (om/set-state! owner preference (.. e -target -checked))
+  )
+
+(defn body [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:include-overview true}
+      )
+    om/IRenderState
+    (render-state [this state]
+      (dom/div nil
+        (dom/h1 nil "Scholars Report")
+        (dom/input
+          #js {:type "checkbox" :checked (:include-overview state)
+               :onChange #(update-preference % owner :include-overview) }
+          "Overview")
+        (dom/div nil
+          (dom/textarea
+            #js {:value (generate-report state)
+                 :rows 20
+                 })
+          )
+        )
+      )
+    ))
 
 (om/root
-  (fn [app owner]
-    (dom/h1 nil "Scholars Report"))
+  body
   app-state
   {:target (. js/document (getElementById "app"))})
+
