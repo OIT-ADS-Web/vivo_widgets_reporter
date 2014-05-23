ee34bb1b04949a37b85c3760213a856352ca5dbb
Patrick McElwee <pmm21@duke.edu>
2014-05-23 14:11:14 -0400

    Give them a nice heading

--- src/vivo_widgets_reporter/core.cljs
+++ src/vivo_widgets_reporter/core.cljs
@@ -11,8 +11,20 @@
   :include-overview true
   }))
 
+(defn create-heading [ {:keys [prefixName firstName lastName]} ]
+  (str "Scholars Report for " prefixName " " firstName " " lastName)
+  )
+
+(defn create-subheading [{:keys [preferredTitle]}]
+  (str preferredTitle)
+  )
+
 (defn set-overview [json owner]
-  (om/set-state! owner :overview (:overview (first (js->clj json :keywordize-keys true))))
+  (let [json-in-clojure (first (js->clj json :keywordize-keys true))]
+    (om/set-state! owner :overview (:overview json-in-clojure))
+    (om/set-state! owner :heading (create-heading json-in-clojure))
+    (om/set-state! owner :subheading (create-subheading json-in-clojure))
+    )
   )
 
 (defn get-overview [owner]
@@ -36,6 +48,8 @@
     (init-state [_]
       {:include-overview true
        :overview ""
+       :heading "Scholars Report"
+       :subheading ""
        }
       )
     om/IWillMount
@@ -45,7 +59,8 @@
     om/IRenderState
     (render-state [this state]
       (dom/div nil
-        (dom/h1 nil "Scholars Report")
+        (dom/h1 nil (:heading state))
+        (dom/p nil (:subheading state))
         (dom/input
           #js {:type "checkbox" :checked (:include-overview state)
                :onChange #(update-preference % owner :include-overview) }
@@ -64,4 +79,3 @@
   body
   app-state
   {:target (. js/document (getElementById "app"))})
-
