eb9023c0e91ddf03053351a3b1d1c9a9a8d06ce2
Patrick McElwee <pmm21@duke.edu>
2014-05-27 17:09:19 -0400

    Book vs journal citations
    Book citations don't have publisher or place information because those
    do not currently come through widgets.

--- src/vivo_widgets_reporter/core.cljs
+++ src/vivo_widgets_reporter/core.cljs
@@ -21,17 +21,30 @@
   (str preferredTitle)
   )
 
-(defn pub-citation [{:keys [label attributes] :as json}]
-  (.log js/console json)
-  (str (:authorList attributes)
-       ". "
-       label
-       " "
-       (:publishedIn attributes)
-       ". "
-       (string/join (take 4 (:year attributes)))
+(defn journal-citation [{:keys [label attributes]}]
+  (str (:authorList attributes) ". "
+       label " "
+       (:publishedIn attributes) " "
+       (:volume attributes) ", no. " (:issue attributes)
+       " (" (string/join (take 4 (:year attributes))) "): "
+       (:startPage attributes) "-" (:endPage attributes)
        "."
        )
+  )
+
+(defn book-citation [{:keys [label attributes]}]
+  (str (:authorList attributes) ". "
+       label ". "
+       (string/join (take 4 (:year attributes))) "."
+       )
+  )
+
+(defn pub-citation [{:keys [vivoType] :as json}]
+  (cond
+    (re-matches #".*AcademicArticle" vivoType) (journal-citation json)
+    (re-matches #".*Book" vivoType) (book-citation json)
+    :else "no such type\n\n"
+    )
   )
 
 (defn parse-labels [json]
@@ -96,13 +109,13 @@
     om/IInitState
     (init-state [_]
       {
-       :faculty-uri "https://scholars.duke.edu/individual/per4284062"
+       :faculty-uri "https://scholars.duke.edu/individual/per2845042"
 
        :heading "Scholars Report"
 
-       :include-overview true
-       :include-appointments true
-       :include-geofoci true
+       :include-overview false
+       :include-appointments false
+       :include-geofoci false
        :include-publications true
        }
       )
